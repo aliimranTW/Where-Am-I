32559626dac0516e9f7a7b88eb7881d84a4d2f19
aliimranTW <aliimrantw@gmail.com>
2020-04-21 21:08:02 +0500

    creating command_robot service

--- catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -1,1 +1,19 @@
-<#include "ros/ros.h">
+#include "ros/ros.h"
+#include <std_msgs/Float64.h>
+#include "ball_chaser/DriveToTarget.h"
+#include <geometry_msgs/Twist.h>
+
+ros::Publisher motor_command_publisher;
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "drive_bot");
+    ros::NodeHandle n;
+    
+    motor_command_publisher = n.adverstise<geometry_msgs::Twist>("/cmd_vel", 10);
+    ros::ServiceServer Service = n.advertiseService("drive_bot/command_robot", handle_drive_request);
+    
+    
+
+}
